366e77cfc30c5bb54214e5d5bbe6c13c12825dd8
Stas Sukora <st.sukora@gmail.com>
2022-11-22 00:48:35 +0300

    Update readme.md1

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,7 +1,9 @@
 package lang.print.gaps.finalModuleTask;
 
+import java.sql.SQLOutput;
+
 public class NumbersPrinter {
     public void printIsPositive(int number) {
-
+        System.out.println(number > 0);
     }
 }
